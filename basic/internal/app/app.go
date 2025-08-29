b442814d25adff62c09ed5895f747ad72eb3ca62
Tuyen Le Chau <tuyenivt@gmail.com>
2025-08-29 20:11:28 +0700

    [basic] create application serving heathcheck api

---
+++ basic/internal/app/app.go
@@ -0,0 +1,25 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Application struct {
+	Logger *log.Logger
+}
+
+func NewApplication() (*Application, error) {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &Application{
+		Logger: logger,
+	}
+	return app, nil
+}
+
+func (*Application) HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
+}
---
+++ basic/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"basic/internal/app"
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Printf("we are running out app on port %d\n", port)
+
+	http.HandleFunc("/health", app.HealthCheck)
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
