15cf069ff2bb2c4a1d2af71b50f18d12e9363cf0
Tuyen Le Chau <tuyenivt@gmail.com>
2025-09-08 08:53:06 +0700

    [basic] update routing for grouping api require authenication and user

--- basic/internal/routes/routes.go
+++ basic/internal/routes/routes.go
@@ -9,14 +9,19 @@ import (
 func SetupRoutes(app *app.Application) *chi.Mux {
 	r := chi.NewRouter()
 
+	r.Group(func(r chi.Router) {
+		r.Use(app.Middleware.Authenticate)
+
+		r.Get("/workouts/{id}", app.Middleware.RequireUser(app.WorkoutHandler.HandleGetWorkoutByID))
+		r.Post("/workouts", app.Middleware.RequireUser(app.WorkoutHandler.HandleCreateWorkout))
+		r.Put("/workouts/{id}", app.Middleware.RequireUser(app.WorkoutHandler.HandleUpdateWorkout))
+		r.Delete("/workouts/{id}", app.Middleware.RequireUser(app.WorkoutHandler.HandleDeleteWorkoutByID))
+
+		r.Get("/users/{username}", app.Middleware.RequireUser(app.UserHandler.HandleGetUserByUsername))
+	})
+
 	r.Get("/health", app.HealthCheck)
 
-	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
-	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
-	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkout)
-	r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkoutByID)
-
-	r.Get("/users/{username}", app.UserHandler.HandleGetUserByUsername)
 	r.Post("/users", app.UserHandler.HandleRegisterUser)
 
 	r.Post("/tokens", app.TokenHandler.HandleCreateNewToken)
