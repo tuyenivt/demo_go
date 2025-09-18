7563da362cda11e2dde5b1aedcf36cb9ddb9a960
Tuyen Le Chau <tuyenivt@gmail.com>
2025-09-18 10:35:35 +0700

    [basic-fullstack] update frontend for error messages

--- basic-fullstack/public/app.js
+++ basic-fullstack/public/app.js
@@ -1,6 +1,22 @@
 window.app = {
   API,
   Router,
+  showError: (
+    message = "There was an error loading the page",
+    goToHome = true
+  ) => {
+    document.querySelector("#alert-modal").showModal();
+    document.querySelector("#alert-modal p").textContents = message;
+    if (goToHome) app.Router.go("/");
+    return;
+  },
+  closeError: () => {
+    document.getElementById("alert-modal").close();
+  },
+  search: (event) => {
+    event.preventDefault();
+    const keywords = document.querySelector("input[type=search]").value;
+  },
 };
 
 window.addEventListener("DOMContentLoaded", () => {
