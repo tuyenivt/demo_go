c195420b631dd04409cb1cf6fc944bf598476ac6
Tuyen Le Chau <tuyenivt@gmail.com>
2025-09-28 16:21:40 +0700

    [concurrency] add multichannel sample

--- concurrency/main.go
+++ concurrency/main.go
@@ -14,6 +14,8 @@ func main() {
 	simpleWaitGroup()
 
 	simpleChannel()
+
+	multiChannel()
 }
 
 func hello(name string) {
@@ -47,4 +49,35 @@ func simpleChannel() {
 
 func chHello(name string, ch chan string) {
 	ch <- fmt.Sprintf("Channel Hello, %s\n", name)
+}
+
+func multiChannel() {
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	ch3 := make(chan string)
+	defer close(ch1)
+	defer close(ch2)
+	defer close(ch3)
+
+	go multiHello(1, ch1)
+	go multiHello(2, ch2)
+	go multiHello(3, ch3)
+
+Loop:
+	for {
+		select {
+		case msg := <-ch1:
+			fmt.Print("MultiChannel 1 - " + msg)
+		case msg := <-ch2:
+			fmt.Print("MultiChannel 2 - " + msg)
+		case msg := <-ch3:
+			fmt.Print("MultiChannel 3 - " + msg)
+			break Loop
+		}
+	}
+}
+
+func multiHello(name int, ch chan string) {
+	time.Sleep(time.Duration(name) * time.Millisecond)
+	ch <- fmt.Sprintf("MultiChannel Hello, %d\n", name)
 }
