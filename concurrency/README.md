16369992e351a4981b6bbf21d9c57c311a875479
Tuyen Le Chau <tuyenivt@gmail.com>
2025-09-29 09:22:19 +0700

    [concurrency] add solution for race condition with NewCond (Wait until cond met and Signal notify ready)

--- concurrency/README.md
+++ concurrency/README.md
@@ -4,3 +4,4 @@
 
 - A concurrency Go practical project.
 - Covering goroutine, waitgroup, channel.
+- Covering solution for race condition with Mutex, atomic, NewCond.
--- concurrency/main.go
+++ concurrency/main.go
@@ -21,6 +21,8 @@ func main() {
 	raceConditionMutex()
 
 	raceConditionAtomic()
+
+	raceConditionNewCond()
 }
 
 func hello(name string) {
@@ -118,7 +120,7 @@ func raceConditionMutexSubtract() {
 	raceConditionMutexWaitGroup.Done()
 }
 
-var raceConditionAtomicShareValue int32 = 1000
+var raceConditionAtomicShareValue int32 = 2000
 var raceConditionAtomicWaitGroup = sync.WaitGroup{}
 
 func raceConditionAtomic() {
@@ -142,4 +144,40 @@ func raceConditionAtomicSubtract() {
 		atomic.AddInt32(&raceConditionAtomicShareValue, -100)
 	}
 	raceConditionAtomicWaitGroup.Done()
+}
+
+var raceConditionNewCondShareValue = 3000
+var raceConditionNewCondWaitGroup = sync.WaitGroup{}
+var raceConditionNewCondMutex = sync.Mutex{}
+var raceConditionNewCondNewCond = sync.NewCond(&raceConditionNewCondMutex)
+
+func raceConditionNewCond() {
+	fmt.Println("raceConditionNewCondShareValue start value = ", raceConditionNewCondShareValue)
+	raceConditionNewCondWaitGroup.Add(2)
+	go raceConditionNewCondAdd()
+	go raceConditionNewCondSubtract()
+	raceConditionNewCondWaitGroup.Wait()
+	fmt.Println("raceConditionNewCondShareValue end value = ", raceConditionNewCondShareValue)
+}
+
+func raceConditionNewCondAdd() {
+	for i := 0; i < 1000; i++ {
+		raceConditionNewCondMutex.Lock()
+		raceConditionNewCondShareValue += 100
+		raceConditionNewCondNewCond.Signal()
+		raceConditionNewCondMutex.Unlock()
+	}
+	raceConditionNewCondWaitGroup.Done()
+}
+
+func raceConditionNewCondSubtract() {
+	for i := 0; i < 1000; i++ {
+		raceConditionNewCondMutex.Lock()
+		for raceConditionNewCondShareValue-100 < 0 {
+			raceConditionNewCondNewCond.Wait()
+		}
+		raceConditionNewCondShareValue -= 100
+		raceConditionNewCondMutex.Unlock()
+	}
+	raceConditionNewCondWaitGroup.Done()
 }
